6eb9b0db11ba8b6ef7d402f92aa73fe0f0776041
Darrick-Oliver <darrickwoliver@gmail.com>
2022-04-15 17:44:49 -0700

    Updated models and predictions

--- src/models/node/index.js
+++ src/models/node/index.js
@@ -2,12 +2,13 @@ const tf = require('@tensorflow/tfjs');
 
 exports.handler = async (event) => {
     bucketName = '100k-points-model'
-    const MODEL_URL = `https://${bucketName}.s3.amazonaws.com/model.json`
+    server = 'us-west-1'
+    const MODEL_URL = `https://${bucketName}.s3.${server}.amazonaws.com/model.json`
 
     const model = await tf.loadLayersModel(MODEL_URL);
 
-    let X = parseFloat(event.X);
-    inputs = tf.tensor(X);
+    let X = event.x;
+    inputs = tf.tensor(X, [1, 51]);
 
     const result = model.predict(inputs);
     const y = (await result.array())[0][0];
